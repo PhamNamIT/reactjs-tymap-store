7df26142fdc471ae6d6f4f7afc09b3d655ef4a2e
PhamNamIT <72678367+PhamNamIT@users.noreply.github.com>
2022-05-25 10:20:32 +0700

    Add files via upload

--- src/routers/Router.jsx
+++ src/routers/Router.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
-import Home from '../pages/Home'
-import Catalog from '../pages/Catalog'
-import Product from '../pages/Product'
-import Cart from '../pages/Cart'
-import Policy from '../pages/Policy'
 import Blog from '../pages/Blog'
 import BlogDetail from '../pages/BlogDetail'
+import Catalog from '../pages/Catalog'
+import Cart from '../pages/Cart'
 import CheckOut from '../pages/CheckOut'
+import Home from '../pages/Home'
+import PolicyDetail from '../pages/PolicyDetail'
+import Product from '../pages/Product'
 
 const Router = () => {
    return (
       <Routes>
-         <Route path="/" element={<Home />} />
-         <Route path="/catalog" element={<Catalog />} />
-         <Route path="/catalog/:slug" element={<Product />} />
-         <Route path="/cart" element={<Cart />} />
-         <Route path="/policy/:slug" element={<Policy />} />
          <Route path="/blog" element={<Blog />} />
          <Route path="/blog/:slug" element={<BlogDetail />} />
+         <Route path="/catalog" element={<Catalog />} />
+         <Route path="/cart" element={<Cart />} />
          <Route path="/check-out" element={<CheckOut />} />
+         <Route path="/" element={<Home />} />
+         <Route path="/policy/:slug" element={<PolicyDetail />} />
+         <Route path="/catalog/:slug" element={<Product />} />
       </Routes>
    )
 }
